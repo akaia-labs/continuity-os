8eb0f8237a559df576c422d77ab7d74370bbb55c
Carina Akaia <cvo.akaia@gmail.com>
2025-05-10 10:28:33 +0000

    wip

--- modules/corvidx/src/entities.rs
+++ modules/corvidx/src/entities.rs
@@ -4,5 +4,4 @@ pub mod foreign_channel;
 pub mod foreign_platform;
 pub mod local_account;
 pub mod message;
-pub mod service;
-pub mod text_channel;
+pub mod message_channel;
--- modules/corvidx/src/entities/service/tables.rs
+++
@@ -1,7 +0,0 @@
-use spacetimedb::{Identity, Timestamp, table};
-
-#[table(name = service, public)]
-pub struct Service {
-	pub name:       Identity,
-	pub registered: Timestamp,
-}
